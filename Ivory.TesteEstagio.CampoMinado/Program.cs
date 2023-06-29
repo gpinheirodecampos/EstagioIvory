f446c3b8124dd633c0dff676543bbc3245dd146e
Gabriel Pinheiro de Campos <93824831+gpinheirodecampos@users.noreply.github.com>
2023-06-28 23:39:45 -0300

    Add files via upload

--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -1,17 +1,122 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ivory.TesteEstagio.CampoMinado
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             var campoMinado = new CampoMinado();
             Console.WriteLine("Início do jogo\n=========");
             Console.WriteLine(campoMinado.Tabuleiro);
+            Console.ReadLine();
 
-            // Realize sua codificação a partir deste ponto, boa sorte!
+            /* Algoritmo capaz de resolver o jogo Campo Minado, cujo tabuleiro é encontrado na classe CampoMinado
+             * Teste para estágio na empresa Ivory IT
+             * 
+             * Autor: Gabriel Pinheiro de Campos
+            */
 
+            // Instanciando classe ResolveCampo
+            var resolveCampo = new ResolveCampo(campoMinado.Tabuleiro);
+
+            // Criando lista que ira armazenar as posicoes das bombas
+            List<Posicao> bombas = new List<Posicao>();
+            var status = 0;
+
+            // Iniciando loop while que tem como condicao de parada o status do jogo estar em "Aberto"
+            while (status == 0)
+            { 
+                var campo = resolveCampo.getCampo();
+
+                // Percorrendo o campo minado
+                for (int x = 0; x < 9; x++)
+                {
+                    for (int y = 0; y < 9; y++)
+                    {
+                        var numFechados = 0;
+                        var numBombas = 0;
+
+                        // Lista que ira armazenar as posicoes dos vizinhos que estao "fechados"
+                        List<Posicao> fechados = new List<Posicao>();
+
+                        // Analisando situacoes em que a casa nao esteja em uma das extremidades do campo e que nao seja a posicao de uma bomba
+                        if (campo[x, y] != "-" && campo[x, y] != "*" && x != 0 && x != 8 && y != 0 && y != 8)
+                        {
+                            // Variavel que ira armazenar as coordenadas de cada vizinho
+                            var vizinhos = resolveCampo.EncontraVizinhos(x, y);
+
+                            // Verifica a situacao de cada vizinho. Caso esteja fechado, incrementa o numero de casas fechadas e adiciona sua posicao na lista de fechados
+                            // Caso o vizinho seja uma bomba, incrementa a variavel que controla o numero de bombas.
+                            foreach (var pos in vizinhos)  
+                            {
+                                if (campo[pos.x, pos.y] == "-")
+                                {
+                                    numFechados++;
+                                    fechados.Add(pos);
+                                }
+                                else if (campo[pos.x, pos.y] == "*")
+                                {  
+                                    numBombas++;
+                                }
+
+                            }
+
+                            // Verifica se o numero da casa analisada no campo é igual a soma do numero das bombas com o numero das posicoes vizinhas fechadas
+                            // Caso positivo, marca os vizinhos como bombas
+                            if (campo[x, y] == (numFechados + numBombas).ToString())
+                            {                                                                  
+                                foreach (var pos in fechados)
+                                {
+                                    campo.SetValue("*", pos.x, pos.y);
+                                    bombas.Add(pos);
+                                }
+                            }
+                            // Caso o numero de bombas seja igual ao numero da casa analisada, realiza a abertura de todos os vizinhos
+                            else if (numBombas.ToString() == campo[x, y])
+                            {   
+                                foreach (var pos in fechados)     
+                                {
+                                    campoMinado.Abrir(pos.x + 1, pos.y + 1);
+                                }
+                            }
+
+                            // Atualiza o Tabuleiro para a próxima iteração
+                            var novo_tabuleiro = campoMinado.Tabuleiro;
+                            resolveCampo.RenovaTabuleiro(novo_tabuleiro);
+                            resolveCampo.MarcaBombas(bombas);
+                        }
+                    }
+                }
+
+                // Atualiza o status do jogo
+                status = campoMinado.JogoStatus;
+            }
+
+            Console.WriteLine("Resultado:\n");
+
+            if (status == 1)
+            {
+                Console.WriteLine("Você ganhou!");
+                Console.WriteLine(campoMinado.Tabuleiro);
+                Console.ReadLine();
+            }
+
+            else if (status == 2)
+            {
+                Console.WriteLine("Você perdeu!");
+                Console.WriteLine(campoMinado.Tabuleiro);
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Tabuleiro Resolvido:");
+            Console.WriteLine(resolveCampo);
+            Console.ReadLine();
+
+            // Fim do programa
         }
+
     }
 }
---
+++ Ivory.TesteEstagio.CampoMinado/ResolveCampo.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ivory.TesteEstagio.CampoMinado
+{
+    public struct Posicao
+    {
+        public int x;
+        public int y;
+
+        public Posicao(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class ResolveCampo
+    {
+        // Propriedades
+        private string _tabuleiro;
+
+        private string[,] campo = new string[9, 9];
+
+        private List<Posicao> fechados = new List<Posicao>() { };
+
+        // Construtor
+        public ResolveCampo(string tabuleiro)
+        {
+            _tabuleiro = tabuleiro;
+
+            LimpaTabuleiro();    //// prepara a String Tabuleiro para popular a matriz
+
+            ConverteTabuleiroParaMatriz();  /// popula a matriz com a String tabuleiro Tratada;
+
+        }
+
+        // Metodos e funcoes
+
+        /// <summary>
+        /// Metodo que limpa e converte para matriz o tabuleiro recebido no parametro.
+        /// </summary>
+        /// <param name="tabuleiro"></param>
+        public void RenovaTabuleiro(string tabuleiro)
+        {
+            _tabuleiro = tabuleiro;
+
+            LimpaTabuleiro();
+
+            ConverteTabuleiroParaMatriz();
+        }
+
+        /// <summary>
+        /// Metodo que atualiza o campo com as posicoes das bombas na lista recebida por parametro.
+        /// </summary>
+        /// <param name="bombas"></param>
+        public void MarcaBombas(List<Posicao> bombas)
+        {
+            foreach (var bomba in bombas)
+            {
+                campo.SetValue("*", bomba.x, bomba.y);
+            }
+        }
+
+        /// <summary>
+        /// Retorna o a matriz do campo
+        /// </summary>
+        /// <returns></returns>
+        public string[,] getCampo()
+        {
+            return campo;
+        }
+
+        /// <summary>
+        /// Retorna uma lista de posicoes das casas que estao fechadas
+        /// </summary>
+        /// <returns></returns>
+        public List<Posicao> getFechados()
+        {
+            return fechados;
+        }
+
+        /// <summary>
+        /// Metodo que identifica as posicoes das casas que estao fechadas e as adiciona na lista "fechados"
+        /// </summary>
+        public void identifica_fechados()
+        {
+            fechados.Clear();
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    if (campo[x, y] == "-")
+                    {
+                        Posicao posicao = new Posicao(x, y);
+                        fechados.Add(posicao);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retira da string tabuleiro elementos como \n e \r
+        /// </summary>
+        public void LimpaTabuleiro()
+        {
+            string tabuleiroLimpo = "";
+            foreach (char value in _tabuleiro)
+            {
+                if (value == '0' || value == '1' || value == '2' || value == '3' || value == '4' || value == '-')
+                {
+                    tabuleiroLimpo += value;
+                }
+            }
+            _tabuleiro = tabuleiroLimpo;
+        }
+
+        public void ConverteTabuleiroParaMatriz()
+        {
+            int item = 0;
+            for (int x = 0; x < 9; x++)
+            {
+
+                for (int y = 0; y < 9; y++)
+                {
+
+                    campo.SetValue(_tabuleiro[item].ToString(), x, y);
+                    item++;
+
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string aux = "";
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    aux += campo.GetValue(x, y);
+                }
+                aux += "\n";
+            }
+            return aux;
+        }
+
+        /// <summary>
+        /// Funcao que recebe as posicoes x e y do elemento e devolve uma lista contendo as posicoes das casas vizinhas a ele
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public List<Posicao> EncontraVizinhos(int x, int y)
+        {
+            List<Posicao> vizinhos = new List<Posicao>() {
+            new Posicao(x - 1, y - 1),
+            new Posicao(x    , y - 1),
+            new Posicao(x + 1, y - 1),
+            new Posicao(x - 1, y),
+            new Posicao(x + 1, y),
+            new Posicao(x - 1, y + 1),
+            new Posicao(x    , y + 1),
+            new Posicao(x + 1, y + 1),
+        };
+            return vizinhos;
+        }
+    }
+}
